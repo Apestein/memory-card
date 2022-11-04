9257fab7a08f2d02fabdf53442428b924403c252
annony <ltn2057@protonmail.com>
2022-11-03 23:16:29 -0400

    add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components"
+import "./styles/index.css"
+import app from "./styles/App.module.css"
 
 function App() {
   const [url, setURL] = useState([])
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [clicked, setClicked] = useState([])
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("https://meme-api.herokuapp.com/gimme/10")
-      const data = await response.json()
-      const urlList = data.memes.map((meme) => meme.url)
-      setURL(urlList)
-    }
-    fetchData()
+    getMemes()
   }, [])
+
+  async function getMemes() {
+    const response = await fetch(
+      "https://meme-api.herokuapp.com/gimme/ProgrammerHumor/15"
+    )
+    const data = await response.json()
+    const urlList = data.memes.map((meme) => meme.url)
+    setURL(urlList)
+  }
+
+  function handleClick(e) {
+    if (clicked.includes(e.target.getAttribute("src"))) {
+      getMemes()
+      setScore(0)
+      setClicked([])
+      return
+    }
+
+    let array = [...url]
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    setURL(array)
+    setScore(score + 1)
+    if (score >= bestScore) setBestScore((bestScore) => bestScore + 1)
+    console.log(e.target.getAttribute("src"))
+    setClicked([...clicked, e.target.getAttribute("src")])
+  }
+
+  const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid #ff006e;
+    color: #ff006e;
+    width: 300px;
+    padding: 10px 0;
+    align-self: center;
+    font-size: large;
+    &:hover {
+      background-color: #ff006e;
+      color: white;
+    }
+  `
 
   return (
     <>
-      <header>
+      <header className={app.header}>
         <h1>Meme-ory</h1>
+        <div className={app.score}>
+          <h3>Click on unique images to gain points</h3>
+          <div>
+            <h3>Score: {score}</h3>
+            <h3>Best Score: {bestScore}</h3>
+          </div>
+        </div>
+        <Button onClick={getMemes}>Get New Memes</Button>
       </header>
-      <main>
+      <main className={app.main}>
         {url.map((URL) => (
-          <img width="250px" key={URL} src={URL} />
+          <img onClick={handleClick} className={app.img} key={URL} src={URL} />
         ))}
       </main>
       <footer></footer>
